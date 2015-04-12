304ac264858b1e8b77373d06f09b1f1b2c26cde0
Kasper Grubbe <kawsper@gmail.com>
2015-04-12 18:03:34 +0100

    New Unicorn master shouldn't kill the old master

--- configs/unicorn.conf.rb
+++ configs/unicorn.conf.rb
@@ -113,12 +113,12 @@ after_fork do |server, worker|
 
 
   # Kill off the new master after forking
-  old_pid = "#{app_dir}/shared/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
+  # old_pid = "#{app_dir}/shared/pids/unicorn.pid.oldbin"
+  # if File.exists?(old_pid) && server.pid != old_pid
+  #   begin
+  #     Process.kill("QUIT", File.read(old_pid).to_i)
+  #   rescue Errno::ENOENT, Errno::ESRCH
+  #     # someone else did our job for us
+  #   end
+  # end
 end
